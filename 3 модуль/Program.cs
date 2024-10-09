9f650fa8f68fdf27da30375a543c4c67fe16c918
Artur <a.durnov@dm-corp.ru>
2024-10-09 22:16:42 +0300

    Размер обуви

--- 3 модуль/Program.cs
+++ 3 модуль/Program.cs
@@ -1,15 +1,29 @@
-﻿namespace _3_модуль
+﻿using System.Xml.Linq;
+
+namespace _3_модуль
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
-           string MyName;
-            MyName = Artur;
-                Console.WriteLine(MyName);
 
-                
+
+            string моеимя = "Artur";
+            byte мойвозраст = 32;
+            bool питомец = true;
+            double размеробуви = 41.5;
+
+            Console.WriteLine("мое имя " + моеимя);
+            Console.WriteLine("мне " + мойвозраст);
+            Console.WriteLine("У тебя есть питомец? " + питомец);
+            Console.WriteLine("мой размер обуви " + размеробуви);
+
+
+
+
+            Console.ReadKey();
+
+
         }
     }
 }
