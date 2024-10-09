f167e6e219215fb8a491851dce9822b19afa2045
Artur <a.durnov@dm-corp.ru>
2024-10-09 21:14:23 +0300

    Добавьте файлы проекта.

---
+++ 3 модуль/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _3_модуль
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+           string MyName;
+            MyName = Artur;
+                Console.WriteLine(MyName);
+
+                
+        }
+    }
+}
