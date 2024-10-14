16125c36558e99fb657173e935473322c8aaaa95
Artur <a.durnov@dm-corp.ru>
2024-10-15 00:29:03 +0300

    юнит 4

--- 3 модуль/Program.cs
+++ 3 модуль/Program.cs
@@ -7,13 +7,31 @@ namespace _3_модуль
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ваше имя: ");
-            var name = Console.ReadLine();
-            Console.WriteLine("ваш возраст: ");
-            var age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Твое имя {0} возраст {1} ", name, age);
-            Console.WriteLine("Ваша дата рождения: ");
-            var birhdate = Console.ReadLine();
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var colore = Console.ReadLine();
+
+            if (colore == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("You colore is Red!");
+            }
+            else if (colore == "Green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("You colore is Green");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("You colore is cyan");
+            }
+
+
+
+
         }
     }
 }
