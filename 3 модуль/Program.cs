8667884b61fe9336bda54b5b6533f2f8c306a4ff
Artur <a.durnov@dm-corp.ru>
2024-10-31 00:37:09 +0300

    Итоговый проект 5.6

--- 3 модуль/Program.cs
+++ 3 модуль/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Drawing;
 using System.Net.NetworkInformation;
 using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 
 namespace _3_модуль
@@ -11,33 +13,58 @@ namespace _3_модуль
         {
             public static void Main(string[] args)
             {
-
-                (string name,string lastname, int age, bool haspet) anketa;
-                Console.Write("Введите имя: ");
-                anketa.name = Console.ReadLine();
-                Console.Write("Введите возраст цифрами: ");
-                anketa.age = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Есть ли у вас питомец? Да или нет");
-                var haspets = true;
-
-                if (haspats==true)
-                {
-                   
-
-                }
-                else 
-                {
-                   
-                }
-
-
-                Console.WriteLine("Ваше имя: {0}", anketa.name);
-                Console.WriteLine("Ваш возраст: {0}", anketa.age);
-                Console.WriteLine("Есть ли у вас питомец?: {0}", anketa.haspet); 
-
-                Console.ReadKey();
-                
+               
+                    var userData = GetUserData();
+                DisplayuserData(userData);
             }
+            static (string Name, string lastname, int Age, string[] petNames ,string[] FavoriteColors) getUserData()
+            {
+             
+                    Console.WriteLine("Введите имя: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию:");
+                string lastname = Console.ReadLine();
+                int Age = GetValidInt("Введите возраст: ");
+
+                Console.WriteLine("Есть ли у вас питомец ? Да или нет: ");
+                bool hasPet = Console.ReadLine().Trim().ToLower() == "да";
+                string[] petNames = null;
+                if (hasPet)
+                {
+                    int peCount = GetValidInt("Введите количество питомцев: ");
+                    petNames = GetValidInt[petCount];
+                }
+                int colorCount = GetValidInt("Введите количество лювимых цветов ");
+                string[] FavoritColors = GetFavoriteColors(colorCount);
+
+                return (name, lastname, Age, petNames, FavoritColors);
+            }
+            static int GetValidInt(string promt)
+            {
+          
+                    int result;
+                while (true)
+                    
+                        Console.Write(promt);
+                if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+                    return result;
+                else
+                    Console.WriteLine("Ошибка: ");
+            }
+            static int GetPetNames(int petCount)
+            {
+                string[] petNames = new string[petCount];
+                for (int i = 0; i < petCount; i++)
+                {
+                    Console.Write($"Введите кличку питомца{i + 1}: ");
+                    petNames[i] = Console.ReadLine();
+                }
+                return petNames;
+            }
+
+
+
         }
     }
 }
